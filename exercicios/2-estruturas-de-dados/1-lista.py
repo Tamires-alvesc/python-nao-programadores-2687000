bca24570e79cfd969fe8b817307acb4eec28dc6e
Tamires Alves <ttamiresacosta@hotmail.com>
2025-05-15 22:56:59 +0000

    projeto resumo ok

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,24 @@
 # Crie uma lista apenas com elementos numéricos
-
+numeros = [2, 7, 9.76, 35, 890, 76, 9]
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
-
+lista = ['Tamires', 123, [1,2,3], 1.23, {'nome': 'Tamires', 'idade': 30}, True, False]
 # Imprima na tela apenas os 5 primeiros elementos da lista
+print(numeros[:5])
+print(lista[:5])
 
 # Crie um slice na lista para que imprima na tela os elementos de índice par
-
+print(numeros[::2])
+print(lista[::2])
 # Remova da lista o último item
-
+numeros.pop()
+lista.pop()
 # Insira na lista um novo item
-
+numeros.append(10)
+lista.append('Oi')
+print(numeros)
+print(lista)
 # Remova da lista um item específico
+numeros.remove(7)
+lista.remove(1.23)
+print(numeros)
+print(lista)
--- exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
+++ exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
@@ -1,5 +1,26 @@
 # Nesse exercício coletaremos dados de uma estudante, armazenaremos em um dicionário e imprimiremos na tela esse dados em um formato amigável.
 
 # 1. Solicite a estudante os seguintes dados: nome, ano que conheceu o LinkedIn, ano atual e os cursos realizados no LinkedIn Learning separados por virgula em ordem cronológica
+nome = input('Entre com o nome:')
+ano_link = int(input('Ano em que conheceu o LinkedIn:'))
+ano_atual = int(input('Ano atual:'))
+cursos = input('Cursos realizados no LinkedIn Learning (separados por vírgula e em ordem cronológica):')
+lista_curso = cursos.split(',')
+
 # 2. Armazene esses dados em um dicionário
-# 3. Imprima na tela uma string com as informações de nome, ano_conheceu_linkedin, total de anos transcurridos, total de cursos realizados e (apenas) o primeiro e último curso.
+dados = dict()
+dados['nome'] = nome
+dados['ano_linkedin'] = ano_link
+dados['ano_atual'] = ano_atual
+dados['cursos'] = lista_curso
+print(dados)
+
+# 3. Imprima na tela uma string com as informações de nome, ano_conheceu_linkedin, total de anos transcorridos, total de cursos realizados e (apenas) o primeiro e último curso.
+print('-'*40)
+print('Informações da estudante:'.center)
+print('-'*40)
+print(f'Nome: {dados["nome"]}')
+print(f'Ano em que conheceu o LinkedIn: {dados["ano_linkedin"]}')
+print(f'Total de anos transcorridos: {dados["ano_atual"] - dados["ano_linkedin"]}')
+print(f'Total de cursos realizados:{len(lista_curso)}')
+print(f'Primeiro e último cursos: {dados["cursos"][0]} e {dados["cursos"][len(lista_curso)-1]} ')
