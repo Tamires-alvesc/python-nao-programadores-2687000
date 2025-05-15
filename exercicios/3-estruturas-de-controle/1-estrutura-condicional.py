554bb88eb059edef18d2c6ac49533c92f7ced96e
Tamires Alves <ttamiresacosta@hotmail.com>
2025-05-15 23:14:45 +0000

    condicional ok

--- exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
+++ exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
@@ -1,19 +1,25 @@
 # Declare 4 variáveis do tipo numérica
-
+a = 0
+b = 2.5
+c = 8
+d = 4.98
 
 # Crie uma estrutura condicional para comparar dois números
-
-
 # Se a condição for verdadeira, imprima na tela uma mensagem informando que a condição foi cumprida e informando o número de maior valor
-
-
 # Se a condição não for cumprida, imprima na tela uma mensagem informando que a condição é negativa e informe o número de maior valor
-
-
 # Insira outras condições na estrutura condicional usando o elif
-
-
+if a > b:
+  print(f'Condição cumprida. O valor {a} é maior do que o valor {b}')
+elif a < b:
+  print(f'Condição negativa. O valor {b} é maior do que o valor {a}')
+elif a == b:
+  print('Valores são iguais')
 # Incremente a estrutura condicional já existente com expressões lógicas utilizando "and" ou "or"
-
+if (a > b) and (c!= 0):
+  print(f'Condição cumprida. O valor {a} é maior do que o valor {b} e {c} é diferente de zero')
+elif (a < b) or (d == 0):
+  print(f'Condição negativa. O valor {b} é maior do que o valor {a} ou {d} é igual a zero')
+else:
+  print('As condições não foram cumpridas')
 
 # Crie uma estrutura condicional onde mais de uma condição seja verdadeira, e use apenas a palavra reservada "if"
--- exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
+++ exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
@@ -1,4 +1,7 @@
-# Nesse desafio você verificará dentro de uma lista se o item estar contido nela, caso verdadeiro deverá imprimir na tela essa informação, além disso deverá solicitar avaliação para o item e armazená-la em um dicionário.
+# Nesse desafio você verificará dentro de uma lista se o item está contido nela,
+#  caso verdadeiro deverá imprimir na tela essa informação, 
+# além disso deverá solicitar avaliação para o item e armazená-la em um dicionário.
+
 # 1. Crie uma lista com 5 diferentes cursos do LinkedIn Learning
 # 2. Crie 3 variáveis do tipo string e associe 1 curso a cada uma delas
 # 3. Crie um dicionário vazio para armazenar a nota do curso
