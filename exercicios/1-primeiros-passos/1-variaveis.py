5aeff081da2b9ce9818ec0141a2d197aaca1b679
Tamires Alves <ttamiresacosta@hotmail.com>
2025-05-15 21:18:39 +0000

    string ok

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,15 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome1 = 'Tamires'
+nome2 = 'Jose'
+senha = 'Tamirinha123'
 # Declare variáveis de cada tipo numérico
+n1 = 2
+n2 = 4.786
+n3 = -5
 
 
 # Declare uma variável do tipo booleano
+bool1 = True
+bool2 = False
--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,7 +3,9 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
 
