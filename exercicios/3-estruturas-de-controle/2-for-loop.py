de8e531a4e739cb192426fe016f383c0dce86027
Tamires Alves <ttamiresacosta@hotmail.com>
2025-05-16 00:03:52 +0000

    fizz buzz ok

--- exercicios/3-estruturas-de-controle/2-for-loop.py
+++ exercicios/3-estruturas-de-controle/2-for-loop.py
@@ -1,9 +1,11 @@
 # Crie uma lista
+lista = [1, 3, 5, 7, 9]
 
 # Crie um for loop para imprimir cada elemento dessa lista
-
+for item in lista:
+  print(item)
 
 # Crie um objeto iterável utilizando a função range()
-
-
 # Use esse objeto iterável para criar um for loop que imprima na tela a frase "Estou aprendendo uma linguagem de programação."
+for k in range(0, 5):
+  print(f'{k} - Estou aprendendo uma linguagem de programação')
--- exercicios/3-estruturas-de-controle/projeto-fizz-buzz.py
+++ exercicios/3-estruturas-de-controle/projeto-fizz-buzz.py
@@ -1,8 +1,22 @@
 # Criaremos um programa para substituir números por palavras em uma lista
 # 1. Crie uma lista com 15 números
+lista = [3, 7, 2, 0, 8, 19, 89, 90, 46, 78, 34, 92, 11, 27, 58]
 # 2. Crie um for loop para percorrer todos os elementos da lista
 # 3. Crie uma estrutura condicional para verificar cada número da lista:
 # 3.1 Caso o número seja divisível por 3, substitua-o por "Fizz"
 # 3.2 Caso o número seja divisível por 5, substitua-o por "Buzz"
 # 3.3 Caso o número seja divisível por 3 e 5, substitua-o por "FizzBuzz"
+indice = 0
 
+for item in lista:
+  if (item % 3 == 0) and (item % 5 == 0):
+    lista[indice] = 'FizzBuzz'
+  elif item % 3 == 0:
+    lista[indice] = 'Fizz'
+  elif item % 5 == 0:
+    lista[indice] = 'Buzz'
+  else:
+    lista[indice] = item
+  indice += 1
+
+print(lista)
